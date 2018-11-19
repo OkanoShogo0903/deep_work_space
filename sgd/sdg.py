6621005e45059c6ad5d25a582b78b8bd5b7deaca
okano_note <okanosyogo@gmail.com>
2018-11-19 16:02:46 +0900

    hoge

--- sgd/sdg.py
+++ sgd/sdg.py
@@ -1,8 +1,25 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 import pprint
 import math
+
+'''
+step1
+- ライブラリのインポート
+step2
+- データセットの読み込み  
+step3
+- データ整形
+step4
+- 学習データとテストデータへの分割
+step5
+- SGD regressorの適応
+step6
+- 結果のプロット
+step7
+- 誤差のプロット
+'''
 
 def sigmoid(t,x):
     ''' return 0~1 '''
@@ -11,12 +28,12 @@ def sigmoid(t,x):
     return h
 
 
-def logstic_regression_predict(t,x):
+def logsticRegressionPredict(t,x):
     ''' return 0 or 1
-    >>> logstic_regression_predict( np.array([1, 3]), np.array([2, 1]))
+    >>> logsticRegressionPredict( np.array([1, 3]), np.array([2, 1]))
     1
-    >>> logstic_regression_predict( np.array([1, 3]), np.array([2, 1]))
-    1
+    >>> logsticRegressionPredict( -1)
+    0
     ''' 
     h = sigmoid(t,x)
     if h >= 0.5:
@@ -38,9 +55,17 @@ class network:
 
 
 # main
+    # データセットの読み込み  
+    # データ整形
     df = pd.read_csv('./housing.data', header=None, sep='\s+')
     df.columns = ['CRIM','ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    # 学習データとテストデータへの分割
+    # Hold out法を使う(8:2に分けるやつ)
+    learn_data  = 
+    test_data   = 
+
     #network = network()
     print(sigmoid( np.array([1, 3]), np.array([2, 1])))
+    print(df) # 506rows,14columns
 
 
